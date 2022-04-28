c1621cace850769ed1f9138976c14b568e5a80be
Vlad <vlad@solidsands.nl>
2022-04-28 16:05:46 +0200

    more stats

--- main.py
+++ main.py
@@ -3,23 +3,31 @@ import re
 import urllib.request
 import pickle
 import colorama
+import numpy as np
 from bs4 import BeautifulSoup
 
 colorama.init()
 tome_link = 'https://te4.org'
 vault_link = f'{tome_link}/characters-vault'
 save_file = 'chars.pickle'
+
+# update filter list
 # page = urllib.request.urlopen(vault_link)
-# with open("temp.html", "w", encoding="utf-8") as fp:
+# with open("filters.html", "w", encoding="utf-8") as fp:
 #     fp.write(page.read().decode('utf-8'))
 #     exit()
 
+
 class VaultSoup:
     def __init__(self):
-        self.soup = BeautifulSoup(open("temp.html", "r", encoding="utf-8").read(), 'html.parser')
+        with open("filters.html", "r", encoding="utf-8") as fp:
+            self.soup = BeautifulSoup(fp.read(), 'html.parser')
         self.filters = {}
         for filter_name in ['difficulty', 'permadeath', 'race', 'class', 'campaign']:
             self.filters[filter_name] = self.get_filter_values(filter_name)
+        # Get the latest game version
+        games = self.get_filter_values('game', get_all=True)
+        self.game = next(iter(games.values()))
         self.save_chars = {}
         try:
             with open(save_file, 'rb') as fp:
@@ -27,17 +35,19 @@ class VaultSoup:
         except:
             pass
 
-    def tag(self, name):
+    @staticmethod
+    def tag(name):
         return f'tag_{name}[]'
 
-    def get_filter_values(self, filter_name):
+    def get_filter_values(self, filter_name, get_all=False):
         values = {}
         for op in self.soup.find('select', {'name': self.tag(filter_name)}).contents:
-            if type(op) is bs4.element.Tag and op.get('data-official'):
+            if type(op) is bs4.element.Tag and (op.get('data-official') or get_all):
                 values[op.contents[0]] = op.get('value')
         return values
 
-    def get_talents(self, talents_type, soup):
+    @staticmethod
+    def get_talents(talents_type, soup):
         talents = soup.find('h4', text=re.compile(talents_type)).parent.find_all('tr')
 
         build = {}
@@ -64,7 +74,8 @@ class VaultSoup:
                 build[cur_cat]['talents'] = {}
         return build
 
-    def get_table(self, table_name, soup, as_list=None):
+    @staticmethod
+    def get_table(table_name, soup, as_list=None):
         title = soup.find('h4', text=re.compile(table_name))
         if not title:
             return None
@@ -85,16 +96,17 @@ class VaultSoup:
         return stats
 
     def request(self, filters, min_level="", max_level="", winner=True,
-                      official_addons=True):
+                official_addons=True):
 
         all_chars = []
+        # TODO: cache pages, but not save all of them like characters
         for page in range(10):
             print(f'scrapping page #{page}')
             req = vault_link + '?'
             req += 'tag_name='
             req += '&tag_level_min=' + str(min_level)
             req += '&tag_level_max=' + str(max_level)
-            req += '&tag_game[]=699172' # current game version tome-1.7.4
+            req += '&tag_game[]=' + str(self.game)
             if winner:
                 req += '&tag_winner=winner'
             if official_addons:
@@ -105,7 +117,9 @@ class VaultSoup:
             req += '&page=' + str(page)
             req += '#'
             print(req)
-            chars = BeautifulSoup(urllib.request.urlopen(req).read(), 'html.parser').find('div', {'id': 'characters'})
+            chars = BeautifulSoup(
+                urllib.request.urlopen(req).read(), 'html.parser'
+            ).find('div', {'id': 'characters'})
             chars = chars.find('tbody').find_all('a')
             if not chars:
                 break
@@ -150,7 +164,7 @@ class VaultSoup:
             build['def_base'] = self.get_table('Defense: Base', soup)
             build['def_res'] = self.get_table('Defense: Resistances', soup)
             build['def_immune'] = self.get_table('Defense: Immunities', soup)
-            
+
             build['prodigies'] = self.get_table('Prodigies', soup, 0)
             build['inscriptions'] = self.get_table('Inscriptions', soup, 1)
 
@@ -195,7 +209,7 @@ def colored_perc(value, maximum, just_color=False, thresholds=[33, 66, 100]):
 
 
 def plot_talents(talents_type, chars):
-    cats = {} # meow
+    cats = {}  # meow
     for char in chars:
         talents = char[talents_type]
         for cat_name in talents:
@@ -204,20 +218,62 @@ def plot_talents(talents_type, chars):
             for talent_name, talent in talents[cat_name]['talents'].items():
                 cat.add(talent_name, talent)
             cats[cat_name] = cat
-    
+
     for cat_name, cat in cats.items():
-        print(cat_name, colored_perc(cat.taken, len(chars)))
+        cat_desc = cat_name + ' ' + colored_perc(cat.taken, len(chars))
+        print(f'{cat_desc:42} 0/5 1/5 2/5 3/5 4/5 5/5')
         for talent, stat in cat.talents.items():
             print(f'\t{talent:25}', end='')
             for i in stat:
-                print(' ' + colored_perc(i, cat.taken, True, [20, 40, 100]) + f'{i:2}' + colorama.Style.RESET_ALL, end='')
+                print(' ' + colored_perc(i, cat.taken, True, [20, 40, 100]) + f'{i:^3}' +
+                      colorama.Style.RESET_ALL, end='')
             print()
         print()
 
 
-# chars = VaultSoup().request({'difficulty': ['insane', 'madness'], 'class': ['brawler']}, winner=True)
-chars = VaultSoup().request({'difficulty': ['insane', 'madness'], 'class': ['brawler']}, min_level=12, max_level=25, winner=False)
-print('\n\n\n\n\t------< CLASS TALENTS >------\n')
-plot_talents('class', chars)
-print('\n\n\t------< GENERIC TALENTS >------\n')
-plot_talents('generic', chars)
+def plot_stats(chars):
+    stats = {}
+    for char in chars:
+        for key, value in char['stats'].items():
+            if key not in stats:
+                stats[key] = {'real': [], 'base': []}
+            # <Stat-name> n (base m)
+            stats[key]['real'].append(int(value.split(' ')[0]))
+            stats[key]['base'].append(int(list(filter(None, re.split(' |\)|\(', value)))[-1]))
+    for key, stat in stats.items():
+        print("{:13} {:5.1f} (base {:5.1f})".format(key, np.average(stat['real']),
+                                                    np.average(stat['base'])))
+
+
+def plot_list(list_type, chars):
+    size = []
+    items = {}
+    for char in chars:
+        size.append(len(char[list_type]))
+        for value in char[list_type]:
+            items[value] = items.get(value, 0) + 1
+    print("Number: {:.1f}".format(np.average(size)))
+    for key, value in sorted(items.items(), key=lambda item: -item[1]):
+        print(key, colored_perc(value, len(chars)))
+
+
+def main():
+    chars = VaultSoup().request({'difficulty': ['insane', 'madness'], 'class': ['brawler']},
+                                winner=True)
+    # chars = VaultSoup().request({'difficulty': ['insane', 'madness'], 'class': ['brawler']},
+    #                             min_level=12, max_level=25, winner=False)
+
+    print('\n\n\n\n\t------< CLASS TALENTS >------\n')
+    plot_talents('class', chars)
+    print('\n\n\t------< GENERIC TALENTS >------\n')
+    plot_talents('generic', chars)
+    print('\n\n\t------< PRIMARY STATS >------\n')
+    plot_stats(chars)
+    print('\n\n\t------< INSCRIPTIONS >------\n')
+    plot_list('inscriptions', chars)
+    print('\n\n\t------< PRODIGIES >------\n')
+    plot_list('prodigies', chars)
+
+
+if __name__ == "__main__":
+    main()
